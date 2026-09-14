2d4ce3d7a75b4e3258734770a1fc76a6346ec7d3
nozaninabduganiyeva4-eng <nozaninabduganiyeva4@gmail.com>
2026-09-14 15:09:56 +0300

    Add main.py entry point for Railway deployment

---
+++ main.py
@@ -0,0 +1,9 @@
+import asyncio
+import logging
+from poem_bot import main
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.info("Bot to'xtatildi!")
